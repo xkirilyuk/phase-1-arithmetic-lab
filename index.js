6a95d67fbdcfde8a3f329baf2933c197956536dc
Xenia <kakirilyuk@gmail.com>
2022-03-08 09:48:25 -0500

    commit tree 7b70107a8880e06c997152e10dd2055146f26794

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+
+let random = Math.ceil(Math.random() * 10 );
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 3, 4, 20);
